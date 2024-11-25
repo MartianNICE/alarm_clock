741c69d5ca6b35ed52547b9e44a2213246d6dccf
MartianNICE <suraj.meruva@gmail.com>
2024-11-25 18:59:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def alarm(seconds):
         time_left = seconds - time_elapsed
         time_left_minutes = time_left // 60
         time_left_seconds = time_left % 60
-        print(f"{CLEAR_AND_RETURN}{time_left_minutes:02d}:{time_left_seconds:02d}")
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {time_left_minutes:02d}:{time_left_seconds:02d}")
 
     playsound("ringtone.mp3")
 minutes = int(input("How many minutes do you want to set the alarm for? "))
